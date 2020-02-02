37309b6e867b4e62d45f1aec288e5bc5ba8a2e41
isamitre <isamitre@gmail.com>
2020-02-02 01:40:14 -0500

    message

--- app/src/main/java/com/example/kissagator/contacts.java
+++ app/src/main/java/com/example/kissagator/contacts.java
@@ -1,31 +1,55 @@
 package com.example.kissagator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.database.Cursor;
+import android.widget.SimpleCursorAdapter;
+import android.provider.ContactsContract;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 
 public class contacts extends AppCompatActivity {
+
+    ListView l1;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        l1 = (ListView)findViewById(R.id.listView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btn = (Button) findViewById(R.id.getContacts);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                get(v);
             }
+
+
         });
+
     }
+
+
+    public void get (View v) {
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+        startManagingCursor(cursor);
+        cursor.moveToNext();
+        String a = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        System.out.println(a);
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone._ID};
+        int[] to = {android.R.id.text1,android.R.id.text2};
+
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,cursor,from,to);
+        l1.setAdapter(simpleCursorAdapter);
+        l1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+    }
+
 }
